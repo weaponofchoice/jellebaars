de3a12df1612d00d5e6b7cf0387d82dcac6b0f44
Luc Awater <lucawater@gmail.com>
2014-10-02 13:51:31 +0200

    retrieved data for single page

--- content/themes/monomark/single.php
+++ content/themes/monomark/single.php
@@ -4,8 +4,31 @@ get_header();
 if( have_posts() ):
   while( have_posts() ): the_post();
   
-    
+  $image_single = get_field('image_single')['url'];
+  $title_single = get_field('title_single');
   
+  // Loop into text_large repeater
+  if( have_rows('text_large_single') ):
+    while( have_rows('text_large_single') ): the_row();
+      $text_large_single = get_sub_field('text');
+    endwhile;
+  endif;
+  
+  // Loop into text_small repeater
+  if( have_rows('text_small_single') ):
+    while( have_rows('text_small_single') ): the_row();
+      $text_small_single = get_sub_field('text');
+    endwhile;
+  endif;
+  
+  ?>
+  
+  <!-- <img src="<?php echo $image_single; ?>"> -->
+  <h2><?php echo $title_single; ?></h2>
+  <p><?php echo $text_large_single; ?></p>
+  <p><?php echo $text_small_single; ?></p>
+  
+  <?php
   endwhile;
 
   else :
