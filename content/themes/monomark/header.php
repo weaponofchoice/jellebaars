fd4d37b5a0464e98701be804b662a7508e2ce3ed
Luc Awater <lucawater@gmail.com>
2014-10-03 16:17:43 +0200

    added basic structural css

--- content/themes/monomark/header.php
+++ content/themes/monomark/header.php
@@ -8,32 +8,24 @@
 <!--[if IE 9]>    <html class="no-js lt-ie10" lang="en"> <![endif]-->
 <!--[if gt IE 9]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Monomark</title>
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/app.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Monomark</title>
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/app.css">
 
-	<!-- WP_HEAD() -->
-	<?php wp_head(); ?>
+  <!-- WP_HEAD() -->
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> id="<?php echo the_title(); ?>">
-	<div id="container">
+  <header>
+    <img src="<?php bloginfo('template_directory'); ?>/img/logo.svg">
+    <a class="hamburger"></a>
+  </header>
+  
+  <div id="container">
     <div class="grid">
       <div class="medium-1 columns"><div></div></div>
       <div class="medium-1 columns"><div></div></div>
       <div class="medium-1 columns"><div></div></div>
     </div>
-    
-    <!-- <div>
-    <?php
-    if( is_front_page() ){
-      echo the_field('header_text', 'option');
-      echo the_field('header_text_2', 'option');
-    }
-    ?>
-  </div> -->
-  <header>
-    <img src="<?php bloginfo('template_directory'); ?>/img/logo.svg">
-    <a class="hamburger"></a>
-  </header>
