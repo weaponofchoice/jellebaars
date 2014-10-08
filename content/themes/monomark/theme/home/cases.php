37555b91dfcceaa517228657654a4aaccf731fd7
Luc Awater <lucawater@gmail.com>
2014-10-08 14:48:04 +0200

    rebuild homepage with dynamic elements

---
+++ content/themes/monomark/theme/home/cases.php
@@ -0,0 +1,4 @@
+<?php
+include( locate_template('theme/home/featured.php') );
+include( locate_template('theme/home/work.php') );
+?>
--- content/themes/monomark/theme/home/text.php
+++ content/themes/monomark/theme/home/text.php
@@ -1,3 +1,4 @@
 <?php
-$intro_text = get_field('intro'); ?>
+$intro_text = get_sub_field('content');
+?>
 <h2 class="margin-b80 medium-2 medium-offset-1 columns"><?php echo $intro_text; ?></h2>
