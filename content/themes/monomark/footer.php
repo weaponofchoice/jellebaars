6b62622f91fb7c7e90cf4b68948daec0bd8cc3e6
Luc Awater <lucawater@gmail.com>
2014-10-08 14:21:34 +0200

    added footer

--- content/themes/monomark/footer.php
+++ content/themes/monomark/footer.php
@@ -1,7 +1,12 @@
-	</div><!-- end of container -->
-	
-	<footer></footer>
-	
-	<?php wp_footer(); ?>
+  </div><!-- end of container -->
+  
+  <footer>
+    <div class="medium-1 medium-offset-1 columns">
+      <h3><?php the_field('footer_text', 'option'); ?></h3>
+      <?php the_field('footer_text_2', 'option'); ?>
+    </div>
+  </footer>
+  
+  <?php wp_footer(); ?>
 </body>
 </html>
