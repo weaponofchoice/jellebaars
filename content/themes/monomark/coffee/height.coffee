1af9d7e64f23d65c33624c33ada0ff155bafed81
Luc Awater <lucawater@gmail.com>
2014-10-09 16:46:14 +0200

    fixed gallery height issue

--- content/themes/monomark/coffee/height.coffee
+++ content/themes/monomark/coffee/height.coffee
@@ -12,10 +12,10 @@ if $('html').length > 0
   gallery.css "height", gallery_height
   
   $(window).on 'resize', ->
-  # Recalculate heights  
-  header_height = header.height()
-  gallery_height = $('.royalSlider .rsMainSlideImage').height()
+    # Recalculate heights  
+    header_height = header.height()
+    gallery_height = $('.rsImg').height()
     
-  # Applys heights
-  header_info.css "margin-top", -header_height
-  gallery.css "height", gallery_height
+    # Applys heights
+    header_info.css "margin-top", -header_height
+    gallery.css "height", gallery_height
--- content/themes/monomark/js/height.js
+++ content/themes/monomark/js/height.js
@@ -9,9 +9,10 @@ if ($('html').length > 0) {
   gallery_height = $('.rsImg').height();
   header_info.css("margin-top", -header_height);
   gallery.css("height", gallery_height);
-  $(window).on('resize', function() {});
-  header_height = header.height();
-  gallery_height = $('.royalSlider .rsMainSlideImage').height();
-  header_info.css("margin-top", -header_height);
-  gallery.css("height", gallery_height);
+  $(window).on('resize', function() {
+    header_height = header.height();
+    gallery_height = $('.rsImg').height();
+    header_info.css("margin-top", -header_height);
+    return gallery.css("height", gallery_height);
+  });
 }
