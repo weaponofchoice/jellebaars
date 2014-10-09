6c241b340f31d169d86927af67031a0c5ef2ed19
Luc Awater <lucawater@gmail.com>
2014-10-09 13:41:53 +0200

    tweaked functions

--- content/themes/monomark/functions.php
+++ content/themes/monomark/functions.php
@@ -3,8 +3,6 @@
  * @package WordPress
  * @subpackage HTML5_Boilerplate
  */
-add_filter('show_admin_bar', '__return_false');
-
 // Renaming some wordpress admin menu items
 add_filter( 'gettext', 'admin_custom_posts' );
 add_filter( 'ngettext', 'admin_custom_posts' );
