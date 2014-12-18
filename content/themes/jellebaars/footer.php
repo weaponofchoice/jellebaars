1793fd6a4a142e14830832c7151b03fe70305e42
Luc Awater <lucawater@gmail.com>
2014-12-18 10:55:55 +0100

    fixed word wrap

--- content/themes/jellebaars/footer.php
+++ content/themes/jellebaars/footer.php
@@ -4,12 +4,6 @@
     <div class="medium-3 medium-offset-0 large-1 large-offset-1 columns">
       <h3><?php the_field('footer_text', 'option'); ?></h3>
       <?php the_field('footer_text_2', 'option'); ?>
-      <div>
-        <a href=""><img src="<?php bloginfo('template_directory'); ?>/img/linkedin_black.svg"></a>
-        <a href=""><img src="<?php bloginfo('template_directory'); ?>/img/vimeo_black.svg"></a>
-        <a href=""><img src="<?php bloginfo('template_directory'); ?>/img/tumblr_black.svg"></a>
-        <a href=""><img src="<?php bloginfo('template_directory'); ?>/img/behance_black.svg"></a>
-      </div>
     </div>
   </footer>
   
